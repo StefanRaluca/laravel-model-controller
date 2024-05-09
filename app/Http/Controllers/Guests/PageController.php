0ed77175d42b399fc4cca1720613e74d683e989d
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-09 14:48:33 +0200

    add movies on the homepage

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+
     public function index()
     {
-        return view("home");
+        $movies = Movie::all();
+        //dd($movies);
+        return view("home", compact("movies"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.app')
 @section('content')
-    <h1>here</h1>
-@endsection
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row row-cols-1 row-cols-md-3">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <h6 class="card-subtitle mb-2">{{ $movie->original_title }}</h6>
+                            <p class="card-text">{{ $movie->nationality }}</p>
+                            <p class="card-text">{{ $movie->date }}</p>
+                            <p class="card-text">{{ $movie->vote }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endsection
